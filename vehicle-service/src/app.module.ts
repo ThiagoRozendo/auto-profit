edbbaff95cd8f427df83845ab1c489153d56db30
MiguelCR000 <miguel.cavalcanti@ufrpe.br>
2026-06-25 19:50:29 -0300

    fix(vehicle-service): restore health check controllers and adjust docker-compose env

--- vehicle-service/src/app.module.ts
+++ vehicle-service/src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { VehiclesModule } from './modules/vehicles/vehicles.module';
 
 @Module({
   imports: [PrismaModule, VehiclesModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
